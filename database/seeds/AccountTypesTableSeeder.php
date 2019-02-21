98ddc65cfd7e74bc8870ab66a6f10eb489973901
Joel Anderson <andejo18@gmail.com>
2019-02-21 15:13:20 -0500

    Modify Seeder for account types

--- database/seeds/AccountTypesTableSeeder.php
+++ database/seeds/AccountTypesTableSeeder.php
@@ -11,6 +11,22 @@ class AccountTypesTableSeeder extends Seeder
      */
     public function run()
     {
-    	factory(App\AccountType::class, 3)->create();
+    	factory(App\AccountType::class)->create([
+    		'account_type' => 'Trial',
+    		'description' => 'Limited access to services.',
+    		'cost' => 00.00
+    	]);
+
+    	factory(App\AccountType::class)->create([
+    		'account_type' => 'Enterprise',
+    		'description' => 'Full access to normal enterprise services.',
+    		'cost' => 100.00
+    	]);
+
+    	factory(App\AccountType::class)->create([
+    		'account_type' => 'Deluxe',
+    		'description' => 'Full access to normal enterprise services plus access to the local zoo amenities.',
+    		'cost' => 1000.00
+    	]);
     }
 }
