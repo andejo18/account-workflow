962e988db347d0d288e56632662685919cd76725
vagrant <vagrant@homestead>
2019-02-21 02:52:04 +0000

    Add accounts table with foreign key

---
+++ database/migrations/2019_02_21_022255_create_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('account_type_id')->unsigned();
+            $table->boolean('active');
+            $table->timestamps();
+
+            $table->foreign('account_type_id')
+                ->references('id')
+                ->on('account_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
